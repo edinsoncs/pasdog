a21c2ee12fb3823090f7448c056a278582724f1f
Edinsoncs <jean_clude_dortosms@hotmail.com>
2018-02-18 22:41:50 -0300

    testing 2

--- backend/socket/init.js
+++ backend/socket/init.js
@@ -14,7 +14,13 @@ module.exports = (io) => {
 			console.log(data);
 
 
-		})
+		});
+
+		client.emit('message', (data) => {
+
+			message: 'Che esto es en tiempo real. la idea es hacer un input y escribir en ello y que envie la data lo que se escriba esto es manual este texto'
+
+		});
 
 	});
 
