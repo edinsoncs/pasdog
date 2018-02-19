36411572f11a56775e07b2a6cc0355b1f72edfe3
Edinsoncs <jean_clude_dortosms@hotmail.com>
2018-02-19 00:27:32 -0300

    in

--- backend/socket/init.js
+++ backend/socket/init.js
@@ -20,7 +20,7 @@ module.exports = (io) => {
 
 
 
-		client.emit('message', (data) => {
+		client.emit('message',  {
 
 			message: 'Che esto es en tiempo real. la idea es hacer un input y escribir en ello y que envie la data lo que se escriba esto es manual este texto'
 
