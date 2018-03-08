acea2781f8a323b9a0f2e769ef50a7cdf4a59069
Edinsoncs <jean_clude_dortosms@hotmail.com>
2018-03-07 22:09:59 -0300

    disconnection 4

--- backend/socket/init.js
+++ backend/socket/init.js
@@ -22,8 +22,10 @@ module.exports.connect = (io) => {
 		client.on('disconnect', function () {
 	        
 	        for(var i=0; i< users.length; i++) {
-	        	
+	        	console.log(users[i].id);
+	        	console.log(client.user.id);
 	            if(users[i].id == client.user.id) {
+	            	console.log('entre');
 	                delete users[users[i]];
 	            }
 	        }
