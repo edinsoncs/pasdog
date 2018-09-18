f59cf9dd9960b36a09cc9b569ab40987100b3c5d
Nicolás Acosta <nadesarrolloweb@gmail.com>
2018-09-18 20:27:44 -0300

    test over map

--- frontend/src/pages/map/map.ts
+++ frontend/src/pages/map/map.ts
@@ -29,10 +29,14 @@ export class MapPage {
 
   map: GoogleMap
   isMapReady: boolean = false
+
+  walkersParsed: any = {}
   walkers: any = {}
   walkersQty: number = 0
+
   subscriptions: any = {
-    markers: null,
+    subscriptions: {},
+    markers: {},
     watch: null
   }
   loadingSpinner: any
@@ -183,7 +187,7 @@ console.log('1- watching')
         longitude:  self.globalProvider.geolocation.longitude,
         user_type: profile.user_type
       })
-    }, 3000)
+    }, 10000)
 
 
 
@@ -224,20 +228,123 @@ console.log('1- watching')
     let self = this,
         profile = JSON.parse(this.globalProvider.getStorage('profile'))
 
+
+
+
+    console.log('update markers', this.walkersParsed)
+
+    // only first
+    if(this.isEmpty(this.walkersParsed)) {
+      this.walkersQty = 0
+
+      //FIXME this.map.clear().then(
+      //FIXME   () => {
+
+          for(let walkerName in this.walkers) {
+            if(this.walkers[walkerName].id != profile.user_id && Number(this.walkers[walkerName].user_type) == 1) {
+              this.walkersQty ++
+              this.walkersParsed[this.walkers[walkerName].id] = this.walkers[walkerName]
+            }
+          }
+
+          for(let walkerId in this.walkersParsed) {
+            // construct map
+            let marker = {
+              icon: {
+                url: `http://maps.google.com/mapfiles/ms/icons/yellow.png`
+              },
+              animation: animation ? 'DROP' : null,
+              position: {
+                lat: self.walkersParsed[walkerId].latitude,
+                lng: self.walkersParsed[walkerId].longitude,
+              }
+            }
+
+            // Now you can use all methods safely.
+            self.map.addMarker(marker).then(mkr => {
+              self.subscriptions.markers[walkerId] = mkr
+
+              self.subscriptions.subscriptions[walkerId] = self.subscriptions.markers[walkerId].on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+                let data = {
+                  id: self.walkersParsed[walkerId].id,
+                  name: self.walkersParsed[walkerId].name,
+                  avatar: self.walkersParsed[walkerId].avatar
+                }
+                self.userPreview(data)
+              })
+            })
+            // construct map
+          }
+
+          console.log('walker parsed: ', this.walkersParsed)
+      //FIXME   }
+      //FIXME )
+
+    }
+
+
+
+
+    else {
+
+      // FIXME: falta verificar el user_type = 1
+
+
+      // busca walkers para eliminar/actualizar
+      for(let walkerId in this.walkersParsed) {
+        const walkerName = this.walkersParsed[walkerId].name
+
+        if(!this.walkers[walkerName]) {
+          console.log('elimina este walker de acá y del mapa (' + walkerId + ')')
+          try {
+            self.subscriptions.markers[walkerId].remove()
+          }
+          catch(e) {
+            console.log('no se pudo eliminar el marker')
+          }
+          delete self.walkersParsed[walkerId]
+        }
+
+        else
+          console.log('calcula posición de este walker para ver si hay que actualizarla (' + walkerId + ')')
+
+      }
+
+
+      // busca walkers para agregar
+      for(let walkerName in this.walkers) {
+        const walkerId = this.walkers[walkerName].id
+
+        if(!this.walkersParsed[walkerId])
+          if(walkerId != profile.user_id)
+            console.log('agrega este walker acá y al mapa (' + walkerId + ')')
+      }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+/*
     this.map.clear().then(
       () => {
-
-        console.log('clear!')
 
         // clean subscriptions
         self.walkersQty = 0
         self.subscriptions.markers ? self.subscriptions.markers.unsubscribe() : null
         const userType = profile.user_type
 
-        if(self.walkers.length)
-          self.walkersQty = self.walkers.length
 
         for(let walker in this.walkers) {
+          self.walkersQty ++
+
           if(self.walkers[walker].id != profile.user_id && Number(self.walkers[walker].user_type) == 1) {
             let marker: MarkerOptions = {
               icon: {
@@ -269,6 +376,8 @@ console.log('1- watching')
 
       }
     )
+    */
+
   }
 
 
@@ -287,6 +396,15 @@ console.log('1- watching')
     }
     let modal = this.modalCtrl.create(UserPreviewPage, data)
         modal.present()
+  }
+
+
+  private isEmpty(obj) {
+    for(var key in obj) {
+      if(obj.hasOwnProperty(key))
+        return false
+    }
+    return true
   }
 
 }
